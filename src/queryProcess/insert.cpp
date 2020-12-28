c81c654f55b59e8ae58722ed5df9f7c08ee9c0b7
prakashupes <85prakash2017@gmail.com>
2020-12-28 16:55:02 +0530

    [src] validation and formation

--- src/queryProcess/insert.cpp
+++ src/queryProcess/insert.cpp
@@ -1,48 +1,114 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<unordered_set>
 #include <fstream>
 #include "../include/insert.hpp"
 #include"../util/util.hpp"
 #include "../include/validateQuery.hpp"
 #include "/usr/local/include/hsql//sql/statements.h"
 using namespace std;
+using namespace hsql;
 namespace dbplus{
+
+    void fillTableColumns(unordered_set<string> & t,string table,vector<string> &sequance)
+    {
+        string file_name="table.db";
     
+        ifstream file;
+        file.open(file_name);
+        if(!file.is_open())cout<<"error"<<endl;
+        string str;
+        vector<string> strings;
+	    while(getline(file,str))
+	    {
+		    strings = split(str,' ');
+            if(strings[0]==table) break;
+
+            strings.clear();
+	    }
+        file.close();
+        for(int i=1;i<strings.size();i+=4)
+        {
+            t.insert(strings[i]);
+            sequance.push_back(strings[i]);
+        }
+
+        /*
+
+        for ( auto it = t.begin(); it != t.end(); ++it )
+             std::cout << " " << *it;
+             */
+
+        
+    }
+    
+    void fillstatementcolumns(const hsql::InsertStatement* stmt, unordered_set<string> & t)
+    {
+
+
+    }
     void Insert::insertStatement(const hsql::InsertStatement* stmt)
         {
             string table(stmt->tableName);
             ValidateQuery valid;
 
-            if(valid.existTable(table))
+            if(!valid.existTable(table))
             {
-              cout<<"Error: Table already exist : "<<table<<endl;
+              cout<<"Error: Table Not exist : "<<table<<endl;
               return;
               
             }
 
             string info=""; //structrure how table info will be stored in the file
-            
-            info+=table;
+            info+=table+" ";
+
+            unordered_set<string> table_columns;
+
+            vector<string>sequance,stmt_columns,values;
+            fillTableColumns(table_columns,table,sequance);
             
 
-    
-             if (stmt->columns != nullptr) {
-      
-             for (char* col_name : *stmt->columns) {
-             cout<<col_name;
+            //fill values
+
+            for (auto expr : *stmt->values) {
+                values.push_back(expr->name);
+        
+      } 
+
+            //fillstatementcolumns();
+
+            //check if providd columns are exist
+            if (stmt->columns != nullptr) {
+                for (char* col_name : *stmt->columns) {
+                    if(table_columns.find(col_name)==table_columns.end())
+                    {
+                        cout<<"error: "<<col_name<<" wrong column\n";
+
+                        return;
+                    }
+                    stmt_columns.push_back(col_name);
+                
+            }
+
+            int j=0;
+            for(int i=0;i<sequance.size();i++)
+            {
+                if(sequance[i]!=stmt_columns[j])
+                {
+                    info=info+"NULL ";
+                }
+                else{
+                    info+=values[j]+" ";
+                    j++;
+                }
             }
          }
 
-      for (auto expr : *stmt->values) {
-        cout<<expr->name<<endl;
-      } 
+         cout<<info;
+
     
         }
         
-    
-    
-
-
 
 }
