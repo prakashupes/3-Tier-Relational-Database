1b563d42212268a1dc2061a9ad95d8b9166f92a8
prakashupes <85prakash2017@gmail.com>
2020-12-29 00:28:19 +0530

    [src] new interface

--- src/main.cpp
+++ src/main.cpp
@@ -21,53 +21,30 @@ int main() {
     string query;
     string err_msg;
     
-    cout<<"Runnig databse DBplus>>\nTo exit from database type q or quit \nFor any kind of help type --help \n\n";
+    cout<<"Runnig databse DBplus>>\nTo exit from database type q or quit \nFor any kind of help type help \n\n";
     while (1)
     {
-        cout<<"DBplus>> ";
+        cout<<"DBplus -: ";
         getline(cin,query);
         if(query=="q" || query=="quit") exit(1);
         if(query.size()<1) continue;
-
-        /**TODO**
-         * Validte query
-         * Execute query
-         * Make data type as enum
-         * 
-        */
-
+        if(query=="h" || query=="help") 
+        {
+            help();
+            continue;
+        }
 
        hsql::SQLParserResult result;
        dbplus::ValidateQuery validateQuery;
        if(!validateQuery.isValid(query,err_msg,result)) 
        {
            cout<<err_msg<<endl;
-           return 0;
+           continue;
        }
 
        ExecuteQuery e;
        e.executeQuery(result);
        
-
-       
-
-
-
-/* TODO
-Execute query will have to parameters 1) stsmt 2)results
-if the query is read type then it return a result
-         
-     /*
-     s
-     
-
-    }
-
-      // dbplus::ValidateQuery::isValid(query,err_msg,qType);
-       //else cout<<"valid"<<endl;
-    
-
-    */
 
 }
     return 0;
