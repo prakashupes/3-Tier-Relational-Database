7b7f002d84b671c9c86237fe0d127e292fe03e46
prakashupes <85prakash2017@gmail.com>
2020-12-29 01:31:52 +0530

    test codes

---
+++ test/Header/header.cpp
@@ -0,0 +1,15 @@
+
+#include "header1.hpp"
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+std::map<std::string ,int> in;
+
+    void Test::insert(){
+        in["a"]=0;
+        in["b"]=1;
+    }
+
+
